5a0c3dfe57557d91bf8cc1bb6309e34fdfaed31d
Christopher J. Bottaro <cjb@cjbotta.ro>
2022-03-11 13:31:54 -0600

    HPUB support

--- lib/nats/protocol/pub.ex
+++ lib/nats/protocol/pub.ex
@@ -1,6 +1,6 @@
 defmodule Nats.Protocol.Pub do
   @moduledoc false
-  defstruct [:subject, :reply_to, :payload, :bytes]
+  defstruct [:subject, :reply_to, :headers, :payload, :bytes]
 
   def new(subject, opts \\ []) do
     {bytes, payload} = process_payload(opts[:payload])
@@ -8,12 +8,13 @@ defmodule Nats.Protocol.Pub do
     %__MODULE__{
       subject: subject,
       reply_to: opts[:reply_to],
+      headers: opts[:headers],
       payload: payload,
       bytes: bytes
     }
   end
 
-  def to_iodata(pub) when is_struct(pub, __MODULE__) do
+  def to_iodata(%__MODULE__{headers: nil} = pub) do
     %{subject: subject, reply_to: reply_to, payload: payload, bytes: bytes} = pub
 
     msg = ["PUB", " ", subject]
@@ -31,7 +32,32 @@ defmodule Nats.Protocol.Pub do
     end
   end
 
-  defp process_payload(nil), do: {0, nil}
+  def to_iodata(%__MODULE__{} = pub) do
+    %{
+      subject: subject,
+      reply_to: reply_to,
+      headers: headers,
+      payload: payload,
+      bytes: bytes
+    } = pub
+
+    reply_to = if reply_to do
+      [" ", reply_to]
+    else
+      ""
+    end
+
+    headers = Enum.map(headers, fn {k, v} ->
+      [k, ": ", v, "\r\n"]
+    end)
+
+    header_size = IO.iodata_length(headers) + 12
+    bytes = header_size + bytes
+
+    ["HPUB ", subject, reply_to, " ", to_string(header_size), " ", to_string(bytes), "\r\n", "NATS/1.0\r\n", headers, "\r\n", payload, "\r\n"]
+  end
+
+  defp process_payload(nil), do: {0, ""}
   defp process_payload(s) when is_binary(s), do: {byte_size(s), s}
   defp process_payload(m) when is_map(m), do: process_payload(Jason.encode!(m))
   defp process_payload(l) when is_list(l), do: process_payload(Map.new(l))
