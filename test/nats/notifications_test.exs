e3fa56b9cc71c2048703563a3a20c2d89adc75d8
Christopher J. Bottaro <cjb@cjbotta.ro>
2022-08-10 20:55:26 -0500

    Cleanup

--- test/nats/notifications_test.exs
+++ test/nats/notifications_test.exs
@@ -39,42 +39,4 @@ defmodule Nats.NotificationsTest do
     send(pid, {what, meta.client})
   end
 
-  # @tag capture_log: true
-  # test "disconnect on monitor" do
-  #   {:ok, conn} = Nats.Client.start_link()
-  #   :ok = Nats.Client.monitor(conn)
-
-  #   socket = Nats.Client.debug(conn).socket
-  #   send(conn, {:tcp_closed, socket})
-
-  #   assert_receive {:nats_client_disconnect, ^conn}
-  # end
-
-  # test "cleans up monitors (start_link)" do
-  #   pid = spawn(fn -> Process.sleep(10_000) end)
-  #   {:ok, conn} = Nats.Client.start_link(monitor: pid)
-  #   Process.exit(pid, :kill)
-
-  #   monitors = Stream.repeatedly(fn -> Nats.Client.debug(conn).notify end)
-  #   |> Stream.with_index()
-  #   |> Enum.reduce_while(nil, fn
-  #     {_monitors, 10}, acc -> {:halt, acc}
-  #     {[], _}, _acc -> {:halt, []}
-  #     {monitors, _}, _acc ->
-  #       Process.sleep(10)
-  #       {:cont, monitors}
-  #   end)
-
-  #   assert [] == monitors
-  # end
-
-  # test "cleans up monitors (monitor)" do
-  #   {:ok, conn} = Nats.Client.start_link()
-
-  #   Task.async(fn -> Nats.Client.monitor(conn) end)
-  #   |> Task.await()
-
-  #   assert [] == Nats.Client.debug(conn).notify
-  # end
-
 end
