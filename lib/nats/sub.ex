a5da095beab524ba36c766b3320e8d53f83c9e9a
Christopher J. Bottaro <cjb@cjbotta.ro>
2022-08-18 16:51:09 -0500

    Jetstream.parse_ack/1

--- lib/nats/sub.ex
+++ lib/nats/sub.ex
@@ -2,6 +2,8 @@ defmodule Nats.Sub do
   @enforce_keys [:subject, :receiver]
   defstruct [:subject, :queue_group, :receiver]
 
+  @type t :: %__MODULE__{}
+
   def new(subject, opts \\ []) do
     %__MODULE__{
       subject: subject,
